ad42657503adc4e1d53e6765ab573d69cb3898ba
Madeline Mocio <>
2019-01-11 13:31:32 -0600

    Chapter 01 - Chapter 04

---
+++ src/Chapter01/P1.java
@@ -0,0 +1,18 @@
+package Chapter01;
+
+/**
+ * Program to out print Welcome to Java!
+ *
+ * @author Madeline Mocio
+ */
+public class P1 {
+
+    /**
+     * Main method
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+    }
+}
